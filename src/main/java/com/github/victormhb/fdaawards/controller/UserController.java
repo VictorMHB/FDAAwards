a4f8ab07e8e3ce3058a4a405bbc7b6880e1f3310
VictorHMB <victor.montanher23@gmail.com>
2025-10-02 02:43:29 -0300

    Adicionando edição de usuarios

--- src/main/java/com/github/victormhb/fdaawards/controller/UserController.java
+++ src/main/java/com/github/victormhb/fdaawards/controller/UserController.java
@@ -2,10 +2,12 @@ package com.github.victormhb.fdaawards.controller;
 
 import com.github.victormhb.fdaawards.dto.user.CreateUserDTO;
 import com.github.victormhb.fdaawards.dto.user.UserDTO;
+import com.github.victormhb.fdaawards.dto.user.UserUpdateDTO;
 import com.github.victormhb.fdaawards.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,9 +38,32 @@ public class UserController {
         return ResponseEntity.ok(userService.findUserById(id));
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+        Long currentUserId = extractCurrentUserId();
+        UserDTO updated = userService.updateUser(id,  dto, currentUserId);
+        return ResponseEntity.ok(updated); //Retorna 200
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<UserDTO> updateUserMe(@Valid @RequestBody UserUpdateDTO dto) {
+        Long currentUserId = extractCurrentUserId();
+        UserDTO updated = userService.updateUser(currentUserId,  dto, currentUserId);
+        return ResponseEntity.ok(updated); //Retorna 200
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build(); //Retorna 204
+    }
+
+    private Long extractCurrentUserId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof com.github.victormhb.fdaawards.model.User user) {
+            return user.getId();
+        }
+
+        throw new RuntimeException("Usuário não autenticado ou principal inválido");
     }
 }
--- src/main/java/com/github/victormhb/fdaawards/service/UserService.java
+++ src/main/java/com/github/victormhb/fdaawards/service/UserService.java
@@ -2,14 +2,17 @@ package com.github.victormhb.fdaawards.service;
 
 import com.github.victormhb.fdaawards.dto.user.CreateUserDTO;
 import com.github.victormhb.fdaawards.dto.user.UserDTO;
+import com.github.victormhb.fdaawards.dto.user.UserUpdateDTO;
 import com.github.victormhb.fdaawards.repository.UserRepository;
 import com.github.victormhb.fdaawards.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -59,6 +62,64 @@ public class UserService {
                 salvo.getUsernameField(),
                 salvo.getEmail(),
                 salvo.getRole().name()
+        );
+    }
+
+    public UserDTO updateUser(Long id, UserUpdateDTO dto, Long currentUserId) {
+        User targetUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new RuntimeException("Usuário autenticado não encontrado"));
+
+        boolean isAdmin = currentUser.getRole() == User.Role.ADMIN;
+
+        if (!id.equals(currentUser.getId()) && !isAdmin) {
+            throw new AccessDeniedException("Você não tem permissão para editaar este usuário");
+        }
+
+        if (dto.getEmail() != null && !dto.getEmail().isEmpty()) {
+            Optional<User> byEmail = userRepository.findByEmail(dto.getEmail());
+            if (byEmail.isPresent() && !byEmail.get().getId().equals(id)) {
+                throw new RuntimeException("Email já em uso por outro usuário.");
+            }
+
+            targetUser.setEmail(dto.getEmail());
+        }
+
+        if (dto.getUsername() != null && !dto.getUsername().isEmpty()) {
+            Optional<User> byUsername = userRepository.findByUsername(dto.getUsername());
+            if (byUsername.isPresent() && !byUsername.get().getId().equals(id)) {
+                throw new RuntimeException("Usuário já em uso por outro usuário.");
+            }
+
+            targetUser.setUsername(dto.getUsername());
+        }
+
+        if (dto.getPassword() != null && !dto.getPassword().isEmpty()) {
+            targetUser.setPassword(passwordEncoder.encode(dto.getPassword()));
+        }
+
+        if (dto.getRole() != null && !dto.getRole().isEmpty()) {
+            if (!isAdmin) {
+                throw new RuntimeException("Somente administradores podem alterar a 'role' de um usuário.");
+            }
+
+            try {
+                User.Role newRole =  User.Role.valueOf(dto.getRole().toUpperCase());
+                targetUser.setRole(newRole);
+            } catch (IllegalArgumentException e) {
+                throw new RuntimeException("Role inválida. Use ADMIN ou VOTER.");
+            }
+        }
+
+        User saved = userRepository.save(targetUser);
+
+        return new UserDTO(
+                saved.getId(),
+                saved.getUsername(),
+                saved.getEmail(),
+                saved.getRole().name()
         );
     }
 
